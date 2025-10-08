86a0d0edaf64d194ce7c07aef808bb2d84ef1a92
DESKTOP-G69AA2G\Administrator <472845337@qq.com>
2025-10-08 16:59:57 +0800

    steam启动游戏方式添加

--- FileUtils.cs
+++ FileUtils.cs
@@ -3,12 +3,15 @@ using System;
 using System.IO;
 
 namespace Utils {
-    public class FileUtils {
-        public static void CreateShortCut(string directory, string shortcutName, string targetPath, string description = null, string iconLocation = null) {
-            if (!Directory.Exists(directory)) {
+    public class FileUtils
+    {
+        public static void CreateShortCut(string directory, string shortcutName, string targetPath, string description = null, string iconLocation = null)
+        {
+            if (!Directory.Exists(directory))
+            {
                 Directory.CreateDirectory(directory);
             }
-            string shortcutPath = GetUnexistLinkName(directory, shortcutName);
+            string shortcutPath = GetUnexistLinkName(directory, shortcutName, ".lnk");
 
             WshShell shell = new WshShell();
             IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
@@ -20,38 +23,89 @@ namespace Utils {
             shortcut.Save();
         }
 
-        public static ShortCutUtil ReadShortCut(string path) {
-            if (System.IO.File.Exists(path)) {
+        public static void CreateUrlShortCut(string directory, string shortcutName, string targetPath, string description = null, string iconLocation = null)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            string shortcutPath = GetUnexistLinkName(directory, shortcutName, ".url");
+
+            WshShell shell = new WshShell();
+            IWshURLShortcut shortcut = (IWshURLShortcut)shell.CreateShortcut(shortcutPath);
+            shortcut.TargetPath = targetPath;
+            shortcut.Save();
+        }
+
+        public static ShortCutUtil ReadShortCut(string path)
+        {
+            if (System.IO.File.Exists(path))
+            {
                 WshShell shell = new WshShell();
                 IWshShortcut shortCut = (IWshShortcut)shell.CreateShortcut(path);
-                ShortCutUtil scu = new ShortCutUtil {
+                ShortCutUtil scu = new ShortCutUtil
+                {
                     FullName = shortCut.FullName,
                     Arguments = shortCut.Arguments,
-                    Description = shortCut.Description, Hotkey = shortCut.Hotkey, TargetPath = shortCut.TargetPath, WindowStyle = shortCut.WindowStyle, WorkingDirectory = shortCut.WorkingDirectory
+                    Description = shortCut.Description,
+                    Hotkey = shortCut.Hotkey,
+                    TargetPath = shortCut.TargetPath,
+                    WindowStyle = shortCut.WindowStyle,
+                    WorkingDirectory = shortCut.WorkingDirectory
 
                 };
                 return scu;
-            } else {
+            }
+            else
+            {
                 return null;
             }
 
         }
 
-        private static string GetUnexistLinkName(string directory, string shortcutName) {
-            string shortcutPath = Path.Combine(directory, string.Format("{0}.lnk", shortcutName));
+        public static UrlShortCut ReadUrlShortCut(string path)
+        {
+            if (System.IO.File.Exists(path))
+            {
+                WshShell shell = new WshShell();
+                IWshURLShortcut shortCut = (IWshURLShortcut)shell.CreateShortcut(path);
+                UrlShortCut scu = new UrlShortCut
+                {
+                    FullName = shortCut.FullName,
+                    Url = shortCut.TargetPath,
+                };
+                return scu;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        private static string GetUnexistLinkName(string directory, string shortcutName, string suffix)
+        {
+            if (string.IsNullOrEmpty(suffix))
+            {
+                suffix = ".lnk";
+            }
+            string shortcutPath = Path.Combine(directory, string.Format("{0}{2}", shortcutName, suffix));
             int index = 1;
-            while (System.IO.File.Exists(shortcutPath)) {
-                shortcutPath = Path.Combine(directory, string.Format("{0} ({1}).lnk", shortcutName, index++));
+            while (System.IO.File.Exists(shortcutPath))
+            {
+                shortcutPath = Path.Combine(directory, string.Format("{0} ({1}){2}", shortcutName, index++, suffix));
             }
             return shortcutPath;
         }
 
-        public static void CreateShortCutOnDesktop(string shortcutName, string targetPath, string description = null, string iconLocation = null) {
+        public static void CreateShortCutOnDesktop(string shortcutName, string targetPath, string description = null, string iconLocation = null)
+        {
             string desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
             CreateShortCut(desktop, shortcutName, targetPath, description, iconLocation);
         }
 
-        public class ShortCutUtil {
+        public class ShortCutUtil
+        {
             public string FullName { get; set; }
             public string Arguments { get; set; }
             public string Description { get; set; }
@@ -60,6 +114,12 @@ namespace Utils {
             public string TargetPath { get; set; }
             public int WindowStyle { get; set; }
             public string WorkingDirectory { get; set; }
+        }
+
+        public class UrlShortCut
+        {
+            public string FullName { get; set; }
+            public string Url { get; set; }
         }
     }
 }
